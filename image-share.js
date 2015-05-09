d44e0be1cc9d07349c07d8eb5577afae717b4965
John Burk <john.burk98@gmail.com>
2015-05-09 13:05:43 -0400

    have file uploads working via autoform

--- image-share.js
+++ image-share.js
@@ -1,13 +1,63 @@
-var Images = new FS.Collection("images", {
+ Images = new FS.Collection("images", {
   stores: [new FS.Store.FileSystem("images", {path: "~/uploads"})]
 });
 
+
+
+Photos = new Mongo.Collection("photos");
+Photos.attachSchema(new SimpleSchema({
+  userId:{
+	type: String,
+	autoValue:function(){return this.userId},
+	
+  },
+  username:{
+	  type: String,
+	  autoValue:function(){return Meteor.users.findOne({_id: this.userId}).username},
+  },
+  
+  groupMembers: {
+    type: String
+  },
+  comments: {
+	type: String  
+  },
+  fileId: {
+    type: String
+  }
+}));
+
+
+Images.allow({
+  download: function () {
+    return true;
+  },
+  fetch: null
+});
+
+if(Meteor.isClient){
+
+Accounts.ui.config({
+	  passwordSignupFields: "USERNAME_ONLY"
+});   
+
+}
+
+
+
+
+/*
 if (Meteor.isClient) {
   
-Template.imagesSubmitted.helpers({
+	Template.imagesS
+	ubmitted.helpers({
    images: Images.find() 
     
 });
+*/
+    
+
+/*    
     
 Template.imagesSubmitted.events({
    
@@ -16,26 +66,36 @@ Template.imagesSubmitted.events({
     Images.remove({_id:this._id});
     
 }
+
     
 });
-    
-Template.main.events({
 
-'change #myFileInput': function(event, template) {
+*/
+
+/*    
+Template.form.events({
+
+'click input[type=submit]': function(event, template) {
+	
+	console.log("form submit")
     event.preventDefault();
+    
     FS.Utility.eachFile(event, function(file) {
-      Images.insert(file, function (err, fileObj) {
+	    
+	    
+      Images.insert(file, function (err, fileObj){
         //Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
       });
          
     });
         
   }   
-    
+  
     
 });
+   
+
+ 
     
     
-    
-    
-}
+*/
